3265bf8320d7458abfb387fe0e15593c8d269470
jereremes <117729768+jereremes@users.noreply.github.com>
2025-05-18 20:04:55 +0000

    Deploying to gh-pages from @ jereremes/jereremes.github.io@2bb31e6b7edd3d1a1842980638752f8f92c24f6e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Studying, refamiliarizing and deepening my understanding of select subjects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
